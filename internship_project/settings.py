0d121f0c8bbff170aeed63593cd9a1b72454853e
Ulan Chokushov <70425821+innokg@users.noreply.github.com>
2022-04-06 11:31:24 +0600

    Update settings.py

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
